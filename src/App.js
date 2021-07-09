cf400f975a651d9235bf28758107e2ee3c8bdc48
psy-g <psykyg@gmail.com>
2021-07-10 08:26:49 +0900

    목업

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Switch, Route, withRouter } from 'react-router-dom';
+import Nav from './containers/NavContainer';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Switch></Switch>
+    </>
   );
-}
+};
 
-export default App;
+export default withRouter(App);
---
+++ src/containers/NavContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+const NavContainer = () => {
+  return <Nav />;
+};
+
+export default NavContainer;
